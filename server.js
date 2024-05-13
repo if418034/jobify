d4accbf9fe16e4254975497413ed50a274f0f027
Faustine Ben Axel Ompusunggu <faustine.b.axel@banksinarmas.com>
2024-05-13 17:52:55 +0700

    added getAllJob

--- server.js
+++ server.js
@@ -1,1 +1,38 @@
-console.log('server is working...')
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+import express from 'express'
+const app = express()
+
+import morgan from 'morgan'
+import { nanoid } from 'nanoid'
+
+let jobs = [
+    {id: nanoid(), company:'apple', position:'front-end'},
+    {id: nanoid(), company:'google', position:'back-end'}
+]
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.post('/', (req, res) => {
+    console.log(req);
+    res.json({message: 'data received', data: req.body})
+})
+
+app.get('/api/v1/jobs', (req, res) => {
+    res.status(200).json({jobs})
+})
+
+const port = process.env.PORT || 5100
+
+app.listen(port, () => {
+    console.log(`Server running on PORT ${port}`);
+})
